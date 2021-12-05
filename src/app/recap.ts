205722232619943af71e1654517d0456eb73a8bf
jefferson marin <jeffersondariom@gmail.com>
2021-12-05 13:25:49 -0500

    add project angular

---
+++ src/app/recap.ts
@@ -0,0 +1,9 @@
+const username : string = 'nicobytes';
+const username2  = 'nicobytes';
+const username3: string | number  = 'nicobytes';
+
+const sum = (a: number,b: number) => {
+  return a+b;
+}
+
+sum(1,3);
