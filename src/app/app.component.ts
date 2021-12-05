cca42dd846ad628fb037922b831b29ac565348d3
jefferson marin <jeffersondariom@gmail.com>
2021-12-05 14:09:54 -0500

    8. Introducción al Event Binding de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   name = 'Nicolas';
   // El acceso debe ser publico, viene publico por defecto
   age = 18;
-  img = 'https://static.bandainamcoent.eu/high/naruto/naruto-suns-revolution/00-page-setup/nsr_game-thumbnail.jpg';
+  img =
+    'https://static.bandainamcoent.eu/high/naruto/naruto-suns-revolution/00-page-setup/nsr_game-thumbnail.jpg';
   btnDisabled = true;
 
   person = {
     name: 'Nicolas',
     age: 18,
-    avatar: 'https://static.bandainamcoent.eu/high/naruto/naruto-suns-revolution/00-page-setup/nsr_game-thumbnail.jpg'
+    avatar:
+      'https://static.bandainamcoent.eu/high/naruto/naruto-suns-revolution/00-page-setup/nsr_game-thumbnail.jpg',
+  };
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+  increaseAge(){
+    this.person.age += 1;
   }
 }
